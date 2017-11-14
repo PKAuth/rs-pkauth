6b276d653bddff73b6ae145a0bcccead9633ea0a
James Parker <dev@jamesparker.me>
2017-11-14 01:10:07 -0500

    lifetime fix: Deserialize -> DeserializeOwned

--- src/sym/enc/mod.rs
+++ src/sym/enc/mod.rs
@@ -8,7 +8,7 @@ pub use crypto_abstract::sym::enc::{gen, Key, Algorithm};
 use ring::error::Unspecified;
 use ring::rand::{SecureRandom, SystemRandom};
 use serde::ser::{Serialize, Serializer, SerializeStruct};
-use serde::de::{Visitor, MapAccess, Deserialize, Deserializer};
+use serde::de::{Visitor, MapAccess, DeserializeOwned, Deserializer};
 use serde_json;
 use std::fmt;
 
@@ -95,8 +95,8 @@ pub fn encrypt<T>( rng : &SystemRandom, key : Key, o : T) -> Result<PKASymEncryp
     encrypt_content( rng, key, r)
 }
 
-pub fn decrypt<'d, T>( key : Key, cipher : PKASymEncrypted) -> Result<T, &'static str> where T:Deserialize<'d> {
-    let d = decrypt_content( &key, &cipher)?;
+pub fn decrypt<T>( key : Key, cipher : PKASymEncrypted) -> Result<T, &'static str> where T:DeserializeOwned {
+    let d : Vec<u8> = decrypt_content( &key, &cipher)?;
     serde_json::from_slice( &d).map_err(|_| "Error parsing json.")
 }
 
@@ -111,13 +111,13 @@ pub fn encrypt_content( rng : &SystemRandom, key : Key, msg : Vec<u8>) -> Result
 
 pub fn decrypt_content( key : &Key, cipher : &PKASymEncrypted) -> Result<Vec<u8>, &'static str> {
     // Make sure the algorithms match.
-    let alg = cipher.algorithm;
-    (ToAlgorithm::to_algorithm( key) == alg).ok_or("Algorithms do not match.")?;
+    let alg = &cipher.algorithm;
+    (&ToAlgorithm::to_algorithm( key) == alg).ok_or("Algorithms do not match.")?;
 
     // Make sure identifiers match.
-    (ToIdentifier::to_identifier( key) == cipher.identifier).ok_or("Key identifiers do not match.");
+    (&ToIdentifier::to_identifier( key) == &cipher.identifier).ok_or("Key identifiers do not match.");
 
-    let c = DecodePSF::decode_psf( &alg, &cipher.ciphertext)?;
+    let c = DecodePSF::decode_psf( alg, &cipher.ciphertext)?;
 
     enc::decrypt( &key, c).map_err(|_| "Error decrypting content.")
 }
