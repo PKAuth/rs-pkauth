187c0d2d8f742099bd3aef7844662fbcecfb4d3d
James Parker <dev@jamesparker.me>
2017-11-20 13:37:41 -0500

    key length check fix

--- src/internal/sym/enc.rs
+++ src/internal/sym/enc.rs
@@ -33,7 +33,7 @@ impl DecodePSF for Key {
     fn decode_psf( alg : &Algorithm, &PSF( ref psf, _) : &PSF<Key>) -> Result<Key, &'static str> where Self : Sized {
         match alg {
             &Algorithm::SEAesGcm256 => {
-                (psf.len() == 32).ok_or("Key is wrong length.");
+                (psf.len() == 32).ok_or("Key is wrong length.")?;
 
                 let mut key = [0u8;32];
 
