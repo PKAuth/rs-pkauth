34b6fd8c62743b8315f5944b351bcc1f5305f29d
James Parker <dev@jamesparker.me>
2017-12-20 02:42:15 -0500

    outlines

--- src/internal/asym/auth.rs
+++ src/internal/asym/auth.rs
@@ -1,9 +1,35 @@
 
 use crypto_abstract::{ToPublicKey};
 use crypto_abstract::asym::auth::{PublicKey, PrivateKey, Algorithm, Signature};
+use serde::ser::{Serialize, Serializer, SerializeStruct};
+use serde::de::{Deserialize, Deserializer, MapAccess, Visitor};
 
 use {ToIdentifier, AlgorithmId};
-use internal::{PKAIdentifier, generate_identifier, EncodePSF, DecodePSF};
+use internal::{PKAIdentifier, generate_identifier, EncodePSF, DecodePSF, PKAJ};
+
+impl<'a> Serialize for PKAJ<&'a PublicKey> {
+    fn serialize<S>( &self, serializer : S) -> Result<S::Ok, S::Error> where S : Serializer {
+        unimplemented!()
+    }
+}
+
+impl<'d> Deserialize<'d> for PKAJ<PublicKey> {
+    fn deserialize<D>( deserializer: D) -> Result<PKAJ<PublicKey>, D::Error> where D : Deserializer<'d> {
+        unimplemented!()
+    }
+}
+
+impl<'a> Serialize for PKAJ<&'a PrivateKey> {
+    fn serialize<S>( &self, serializer : S) -> Result<S::Ok, S::Error> where S : Serializer {
+        unimplemented!()
+    }
+}
+
+impl<'d> Deserialize<'d> for PKAJ<PrivateKey> {
+    fn deserialize<D>( deserializer: D) -> Result<PKAJ<PrivateKey>, D::Error> where D : Deserializer<'d> {
+        unimplemented!()
+    }
+}
 
 impl ToIdentifier for PublicKey {
     fn to_identifier( key : &PublicKey) -> PKAIdentifier {
